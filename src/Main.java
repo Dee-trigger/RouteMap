344f6c548a76e718e191efd9be88b45cd1274d7f
Dee-trigger <320156271@qq.com>
2025-05-24 11:03:23 +0800

    重构工程
    idea默认工程不方便导入外部库

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-       float a = 1.2f;
-       a++;
-       System.out.println(a);
-
-        System.out.println("Hello world!");
-    }
-}
